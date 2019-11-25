18b2ae7cc451ca6f4ece11563d5a9ac9e51f3c67
Bosco1020 <55706703+Bosco1020@users.noreply.github.com>
2019-11-25 15:43:11 +0000

    main character donoo
    He moves, He spins, He shoots pew pew pew bins, it's ur boiiii, The saviour boiiii

---
+++ Tower Defense Shooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float moveSpeed = 1000.0f;
+    public int damage = 1;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(transform.up * moveSpeed);   
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        Die();
+    }
+    private void OnBecomeInvisible()
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
