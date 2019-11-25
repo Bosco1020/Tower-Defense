dfa8775ef065be0bad89a54e8bb16b1d443bc4ea
Bosco1020 <55706703+Bosco1020@users.noreply.github.com>
2019-11-25 16:32:05 +0000

    Devan!
    00Devan moves, he follows me even if a little janky

---
+++ Tower Defense Shooter/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnDamageEvent : UnityEvent<int> {}
+
+public class HealthSystem : MonoBehaviour
+{
+    public int health = 10;
+    public UnityEvent onDie;
+    public OnDamageEvent onDamaged;
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        onDamaged.Invoke(health);
+
+        if (health < 1)
+        {
+            onDie.Invoke();
+        }
+    }
+}
