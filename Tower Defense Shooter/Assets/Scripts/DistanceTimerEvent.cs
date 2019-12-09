6b9f59c19f599bd581a532afe375aa0b4617d91a
Bosco1020 <55706703+Bosco1020@users.noreply.github.com>
2019-12-09 17:15:07 +0000

    created distance timer

---
+++ Tower Defense Shooter/Assets/Scripts/DistanceTimerEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DistanceTimerEvent : MonoBehaviour
+{
+    public float time = 5, Target = 4;
+    private float Distance;
+    public Transform target, self;
+    public bool repeat = false;
+    public UnityEvent onTimerComplete;
+
+    public void Start()
+    {
+        if (repeat)
+        {
+            Distance = Vector3.Distance(target.position, self.position);
+            if (Distance < Target)
+            {
+                InvokeRepeating("OnTimerComplete", 0, time);
+            }
+            
+        }
+        else
+        {
+            Invoke("OnTimerComplete", time);
+        }
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+    private void OnTimerComplete()
+    {
+        onTimerComplete.Invoke();
+    }
+}
