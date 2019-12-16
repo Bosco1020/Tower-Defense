b02b1f3f13050a430555db9466c1adcd701d4004
Bosco1020 <55706703+Bosco1020@users.noreply.github.com>
2019-12-16 04:10:31 +0000

    enemy range
    Enemies will only shot within range on the player

--- Tower Defense Shooter/Assets/Scripts/Enemy.cs
+++ Tower Defense Shooter/Assets/Scripts/Enemy.cs
@@ -6,9 +6,32 @@ public class EnemySpawnedEvent : UnityEvent<Transform> { }
 public class Enemy : MonoBehaviour
 {
     public EnemySpawnedEvent onSpawn;
+    public float range = 5f;
+    public float rate = 0.5f;
+    public UnityEvent ifInRange;
+    GameObject player;
+
     private void Start()
     {
-        GameObject player = GameObject.FindWithTag("Player");
+        player = GameObject.FindWithTag("Player");
         onSpawn.Invoke(player.transform);
+        InvokeRepeating("InRange", 0f, rate);
     }
+    void InRange()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        
+        if (distanceToPlayer <= range)
+        {
+            ifInRange.Invoke();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+
 }
